30c2617e610057e631cfca90210955d4a0360990
Shruti Jha <133661363+jha15534@users.noreply.github.com>
2025-08-11 12:49:13 +0530

    Initial commit: Higher Lower Game

---
+++ Day14/game_data.py
@@ -0,0 +1,122 @@
+data = [
+    {
+        "name": "Virat Kohli",
+        "follower_count": 278,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Priyanka Chopra Jonas",
+        "follower_count": 91,
+        "description": "Actress and singer",
+        "country": "India"
+    },
+    {
+        "name": "Alia Bhatt",
+        "follower_count": 87,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Shraddha Kapoor",
+        "follower_count": 88,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Narendra Modi",
+        "follower_count": 80,
+        "description": "Prime Minister of India",
+        "country": "India"
+    },
+    {
+        "name": "MS Dhoni",
+        "follower_count": 50,
+        "description": "Former cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Sachin Tendulkar",
+        "follower_count": 45,
+        "description": "Former cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Deepika Padukone",
+        "follower_count": 80,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Ranveer Singh",
+        "follower_count": 45,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Jasprit Bumrah",
+        "follower_count": 14,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Katrina Kaif",
+        "follower_count": 77,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Akshay Kumar",
+        "follower_count": 66,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Rohit Sharma",
+        "follower_count": 35,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Neha Kakkar",
+        "follower_count": 75,
+        "description": "Singer",
+        "country": "India"
+    },
+    {
+        "name": "Anushka Sharma",
+        "follower_count": 64,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Shah Rukh Khan",
+        "follower_count": 47,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Salman Khan",
+        "follower_count": 67,
+        "description": "Actor",
+        "country": "India"
+    },
+    {
+        "name": "Madhuri Dixit",
+        "follower_count": 42,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "Kriti Sanon",
+        "follower_count": 55,
+        "description": "Actress",
+        "country": "India"
+    },
+    {
+        "name": "KL Rahul",
+        "follower_count": 17,
+        "description": "Cricketer",
+        "country": "India"
+    }
+]
